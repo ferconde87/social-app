7afe97a3e3d14ced3955910d3c10b5e1cbbadd88
fer <ferconde87@gmail.com>
2021-07-06 23:30:31 +0700

    User signup + flash and error  messages + test integration signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Social!"
+      redirect_to @user
+    else
+      render 'users/new'
+    end
+  end
+
+  private
+
+    def user
+      @user ||= User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
