be736789814e482b02efc91a1e6d2d425f950668
fer <ferconde87@gmail.com>
2021-07-25 12:32:24 +0700

    Migration for PG google_id & facebook_id as indeces

--- db/migrate/20210723082440_add_google_and_facebook_to_users.rb
+++ db/migrate/20210723082440_add_google_and_facebook_to_users.rb
@@ -2,7 +2,7 @@ class AddGoogleAndFacebookToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :google_id, :string
     add_column :users, :facebook_id, :string
+    add_index :users, :google_id
+    add_index :users, :facebook_id
   end
-  add_index :users, :google_id
-  add_index :users, :facebook_id
 end
