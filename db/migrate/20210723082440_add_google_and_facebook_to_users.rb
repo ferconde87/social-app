562a3ee37258ec893c778be8397d724a8ba67860
fer <ferconde87@gmail.com>
2021-07-25 12:21:50 +0700

    google_id:string & facebook_id: string

--- db/migrate/20210723082440_add_google_and_facebook_to_users.rb
+++ db/migrate/20210723082440_add_google_and_facebook_to_users.rb
@@ -1,7 +1,7 @@
 class AddGoogleAndFacebookToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :google_id, :Integer
-    add_column :users, :facebook_id, :Integer
+    add_column :users, :google_id, :string
+    add_column :users, :facebook_id, :string
   end
   add_index :users, :google_id
   add_index :users, :facebook_id
--- db/migrate/20210724074553_change_column_google_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnGoogleIdToUsers < ActiveRecord::Migration[6.1]
-  def change
-    change_column :users, :google_id, :string
-  end
-end
