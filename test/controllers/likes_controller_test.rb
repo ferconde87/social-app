3857adbe6d8e3c1154fdd9629475fb171d1964fc
fer <ferconde87@gmail.com>
2021-08-02 15:11:58 +0700

    Test for Like functionality

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,7 +1,93 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:fernando)
+    @pepe_post1 = posts(:hola)
+    @pepe_post2 = posts(:moni)
+    @pepe = users(:pepe)
+    log_in_as(@user)
+  end
+
+  test "like a post" do
+    assert_not @user.like? @post
+    assert_equal  @pepe.posts.length, 2
+    get user_path(@pepe)
+    assert_template 'users/show'
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-up', text: "0"
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', count: 0
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 0
+    assert_difference 'Like.count', 1 do
+      post "/like_post/#{@pepe_post1.id}"
+    end
+    get user_path(@pepe)
+    assert_template 'users/show'
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', text: "1"
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 0
+    #pepe also like his post
+    log_in_as(@pepe)
+    assert_difference 'Like.count', 1 do
+      post "/like_post/#{@pepe_post1.id}"
+    end
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', text: "2"
+  end
+
+  test "dislike a post" do
+    assert_not @user.dislike? @pepe_post2
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 0
+    assert_difference 'Like.count' do
+      post "/dislike_post/#{@pepe_post1.id}"
+    end
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 1
+  end
+
+  test "cancel a like" do
+    post "/like_post/#{@pepe_post1.id}"
+    assert @user.like? @pepe_post1
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', text: "1"
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 1
+    
+    assert_difference 'Like.count', -1 do
+      post "/cancel_like/#{@pepe_post1.id}"
+    end
+    
+    assert_not @user.like? @pepe_post1
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', count: 0
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-up', text: "0"
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 2
+  end
+
+  test "cancel a dislike" do
+    post "/dislike_post/#{@pepe_post1.id}"
+    assert @user.dislike? @pepe_post1
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', text: "1"
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 1
+    
+    assert_difference 'Like.count', -1 do
+      post "/cancel_dislike/#{@pepe_post1.id}"
+    end
+    
+    assert_not @user.dislike? @pepe_post1
+    get user_path(@pepe)
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 0
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-down', text: "0"
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 2
+  end
 end
--- test/integration/user_likes_post_test.rb
+++ test/integration/user_likes_post_test.rb
@@ -2,20 +2,58 @@ require "test_helper"
 
 class UserLikesPostTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:fernando)
+    @fer = users(:fernando)
     @post = posts(:hola)
+    @michael = users(:michael)
+    @zone = posts(:zone)
+    log_in_as(@fer)
   end
 
   test "user likes a post" do
-    assert_not @user.like?(@post)
+    assert_not @fer.like?(@post)
     likes_before = @post.likes_count
     dislikes_before = @post.dislikes_count
     assert_difference 'Like.count', 1 do
-      @user.like_post @post
+      @fer.like_post @post
     end
-    assert @user.like?(@post)
+    assert @fer.like?(@post)
     @post.reload
     assert_equal likes_before+1, @post.likes_count
     assert_equal dislikes_before, @post.dislikes_count
   end
+
+  test "user likes a post with other likes then dislikes it" do
+    assert_not @fer.like?(@zone)
+    get user_path(@michael)
+    assert_template 'users/show'
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', count: 0
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-up', text: "2"
+    assert_difference '@fer.posts_liked.length', 1 do
+      @fer.like_post @zone
+    end
+    assert @fer.like?(@zone)
+    get user_path(@michael)
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-up-fill', text: "3"
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 1
+    
+    # TODO update Like instead of using post_liked & post_not_liked
+    assert_not @fer.dislike? @zone
+    assert_select 'i.bi.bi-hand-thumbs-down', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-down', text: "1"
+    
+    assert_no_difference 'Post.count' do
+      @fer.dislike_post @zone
+    end
+    
+    assert @fer.dislike? @zone
+    get user_path(@michael)
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', count: 1
+    assert_select 'i.bi.bi-hand-thumbs-down-fill', text: "2"
+    assert_select 'i.bi.bi-hand-thumbs-up', count: 2
+    assert_select 'i.bi.bi-hand-thumbs-up', text: "2"
+  end
+
+
 end
