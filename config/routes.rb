d7d8d79ebfaa0020a1511354b4490b0c23adf9c5
fer <ferconde87@gmail.com>
2021-08-03 17:25:52 +0700

    Privacy Policy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
+  get '/privacy', to: 'static_pages#privacy'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
