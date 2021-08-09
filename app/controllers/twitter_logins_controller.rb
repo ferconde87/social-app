2c115c8f6e696c23a3e61e6f93a9b1412d691adf
fer <ferconde87@gmail.com>
2021-08-09 10:52:03 +0700

    Twitter sign in: updating existing user with the twitter_id

--- app/controllers/twitter_logins_controller.rb
+++ app/controllers/twitter_logins_controller.rb
@@ -3,18 +3,20 @@ class TwitterLoginsController < LoginsController
   end
   
   def create
-    user = User.find_by(twitter_id: auth_hash.uid)
+    id = auth_hash.uid
+    user = User.find_by(twitter_id: id)
     email = auth_hash.info.email
     if user.nil? && !email.nil?
       if user = User.find_by(email: email)
+        debugger
         user.activate_with_atttribute("twitter_id", id)
       end
     end
     if user.nil?
       user_info = {}
       user_info[:name] = auth_hash.info.name
-      user_info[:email] = email.nil? ? "#{auth_hash.uid}@fb.com"  : email
-      user_info[:twitter_id] = auth_hash.uid
+      user_info[:email] = email.nil? ? "#{id}@fb.com"  : email
+      user_info[:twitter_id] = id
       user = create_user(user_info)
       login(user, signup_message)
     else
